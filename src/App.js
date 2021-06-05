60f89ae1d47948df68c377c2189ba1eeab7f4aee
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-06-05 17:22:13 -0500

    working model example in place, time to tinker 2 learn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('John Doe');
+
+  const alertName = () => {
+    alert(name);
+  };
+
+  const handleNameInput = e => {
+    setName(e.target.value);
+  };
+
   return (
-    <div className="App">
-      Blank App
+    <div>
+      <h3>This is a Functional Component</h3>
+      <input
+        type="text"
+        onChange={handleNameInput}
+        value={name}
+        placeholder="Your Name"
+      />
+      <button onClick={alertName}>
+        Alert
+      </button>
     </div>
   );
-}
+};
 
 export default App;
