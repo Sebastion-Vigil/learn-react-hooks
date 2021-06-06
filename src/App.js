22955780f0509bb22f4cd3a136643aa9288f2c5c
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-06-06 17:43:00 -0500

    hooks r fun

--- src/App.js
+++ src/App.js
@@ -1,62 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import './App.css'
 
-const App = () => {
-  const [userName, setUsername] = useState('johndoe')
-  const [firstName, setFirstname] = useState('John')
-  const [lastName, setLastname] = useState('Doe')
+function App() {
+  const [header, setHeader] = useState('Time 2 Learn React Hooks!');
 
   useEffect(() => {
-    setInterval(() => {
-      setUsername('janedoe')
-      setFirstname('Jane')
-      setLastname('Doe')
-    }, 5000)
-  })
+    const newheader = document.querySelectorAll('#header')[0];
+    setTimeout(() => {
+      newheader.innerHTML = header;
+    }, 3000);
+  });
 
-  const logName = () => {
-    console.log(userName)
-    console.log(firstName)
-    console.log(lastName)
-  }
-
-  const handleUserNameInput = e => {
-    setUsername({ userName: e.target.value })
-  }
-  const handleFirstNameInput = e => {
-    setFirstname({ firstName: e.target.value })
-  }
-  const handleLastNameInput = e => {
-    setLastname({ lastName: e.target.value })
-  }
+  const handleHeaderInput = e => {
+    setHeader(e.target.value);
+  };
 
   return (
     <div className='App'>
-      <h3>Time 2 Learn React Hooks!</h3>
+      <h3 id="header">This is a Functional Component</h3>
       <input
-        type='text'
-        onChange={handleUserNameInput}
-        value={userName}
-        placeholder='Your Username'
+        type="text"
+        onChange={handleHeaderInput}
+        value={header}
       />
-      <input
-        type='text'
-        onChange={handleFirstNameInput}
-        value={firstName}
-        placeholder='Your First Name'
-      />
-      <input
-        type='text'
-        onChange={handleLastNameInput}
-        value={lastName}
-        placeholder='Your Last Name'
-      />
-      <button className='btn btn-large right' onClick={logName}>
-        Log Names
-      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
