ca77a55f7a6c3388452f2b2686ec03830c326f95
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-06-06 17:19:45 -0500

    another fun example with useEffect, getting the hang of it

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
 
 import './App.css'
 
 const App = () => {
-  const [name, setName] = useState('');
+  const [userName, setUsername] = useState('johndoe')
+  const [firstName, setFirstname] = useState('John')
+  const [lastName, setLastname] = useState('Doe')
 
-  const alertName = () => {
-    alert(name);
-    setName('')
-  };
+  useEffect(() => {
+    setInterval(() => {
+      setUsername('janedoe')
+      setFirstname('Jane')
+      setLastname('Doe')
+    }, 5000)
+  })
 
-  const handleNameInput = e => {
-    setName(e.target.value);
-  };
+  const logName = () => {
+    console.log(userName)
+    console.log(firstName)
+    console.log(lastName)
+  }
+
+  const handleUserNameInput = e => {
+    setUsername({ userName: e.target.value })
+  }
+  const handleFirstNameInput = e => {
+    setFirstname({ firstName: e.target.value })
+  }
+  const handleLastNameInput = e => {
+    setLastname({ lastName: e.target.value })
+  }
 
   return (
     <div className='App'>
       <h3>Time 2 Learn React Hooks!</h3>
       <input
-        type="text"
-        onChange={handleNameInput}
-        value={name}
-        placeholder="Type your name"
+        type='text'
+        onChange={handleUserNameInput}
+        value={userName}
+        placeholder='Your Username'
       />
-      <button onClick={alertName}>
-        Smash!
+      <input
+        type='text'
+        onChange={handleFirstNameInput}
+        value={firstName}
+        placeholder='Your First Name'
+      />
+      <input
+        type='text'
+        onChange={handleLastNameInput}
+        value={lastName}
+        placeholder='Your Last Name'
+      />
+      <button className='btn btn-large right' onClick={logName}>
+        Log Names
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
